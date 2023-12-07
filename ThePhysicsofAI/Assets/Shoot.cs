0aa82cb43f04cee55302c7b68e1c3d45bc435b26
TheFALCON736 <710228@student.dusd.net>
2023-12-07 11:42:23 -0800

    Almost Done
    Almost done

---
+++ ThePhysicsofAI/Assets/Shoot.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    public GameObject shellPrefab;
+    public GameObject shellSpawnPos;
+    public GameObject target;
+    public GameObject parent;
+    float speed = 15;
+    float turnSpeed = 2;
+
+    
+    void Start()
+    {
+        
+    }
+
+    void Fire()
+    {
+        GameObject shell = Instantiate(shellPrefab, shellSpawnPos.transform.position, shellSpawnPos.transform.rotation);
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            Fire();
+
+        Vector3 direction = (target.transform
+    }
+}
